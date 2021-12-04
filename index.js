c4964be6d5c81fa9e4dcd92e5cc9f7f57a57cdd4
Matthew <matthewnakayama@gmail.com>
2021-12-04 10:58:55 -0800

    removed loadTOMLConfig

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-import loadTOMLConfig from "./libs/loadTOMLConfig.js";
 import launchSkeletonServer from "./skeletonServer.js";
 import loadConfig from './loadConfig.js';
-
-const configFile = loadTOMLConfig("./config.toml");
 
 Object.keys(configFile.servers).forEach(serverName => {
     console.log("Loading the server " + serverName);
